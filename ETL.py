8d801c08c81d3f081bb11e4cc34b0aba9323f077
Rajat Kumar <rajatkumar39008@gmail.com>
2026-06-04 20:54:42 +0530

    Add performance data loading functions to ETL module

--- ETL.py
+++ ETL.py
@@ -31,3 +31,54 @@ def load_students_details():
         cursor.close()
         conn.close()
         print("Data loaded successfully into students table")
+
+
+
+        
+def load_performance_input():
+        conn = get_connection()
+        curr = conn.cursor()
+
+        with open(r"F:\Youtube\ANALYST MIND Project 1\ Student Learning Analytics System Project\PerformanceInput.json") as f:
+            data = json.load(f)
+
+            for row in data:
+                curr.execute("""INSERT INTO performance_input (attempt_id,child_id,school_id,submitted_at,status)
+                            values(%s,%s,%s,%s,%s) on conflict (attempt_id) do nothing""",
+                            (
+                                row.get('id'),
+                                row.get('child_id'),
+                                row.get('school_id'),
+                                row.get('submitted_at'),
+                                row.get('status')
+                            )
+                )
+        conn.commit()
+        curr.close()
+        conn.close()
+        print("Data loaded successfully into performance_input table")
+    
+
+def load_performance_metrics():
+    conn = get_connection()
+    curr = conn.cursor()
+    with open(r"F:\Youtube\ANALYST MIND Project 1\ Student Learning Analytics System Project\PerformanceMetrics.json") as f:
+        data = json.load(f)
+        for row in data:
+              curr.execute("""INSERT INTO performance_metrics (metric_id,input_id,processed_date,wpm,wcpm,pronunciation,fluency,noise)
+                           values(%s,%s,%s,%s,%s,%s,%s,%s) on conflict (metric_id) do nothing""",
+                           (
+                                row.get('id'),
+                                row.get('input_id'),
+                                row.get('processed_date'),
+                                row.get('wpm'),
+                                row.get('wcpm'),
+                                row.get('pronunciation'),
+                                row.get('fluency'),
+                                row.get('noise')
+                                )
+                                )
+    conn.commit()
+    curr.close()
+    conn.close()
+    print("Data loaded successfully into performance_metrics table")
